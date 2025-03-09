471503dff6c694a957be563515f42eab4a380ab5
AUMNSG-Creators29 <nirnaysgodlaw@gmail.com>
2025-03-09 07:02:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Toggle mobile menu
+const navLinks = document.querySelector('.nav-links');
+const ctaButton = document.querySelector('.cta-button');
 
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  ctaButton.classList.toggle('active');
+});
